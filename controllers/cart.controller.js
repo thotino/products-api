3d406db121f04507cbf2f2c830599b649ddf2ea9
Thotino GOBIN-GANSOU <gthomario@yahoo.com>
2023-01-02 16:05:42 +0100

    feat: models usage in controllers

--- controllers/cart.controller.js
+++ controllers/cart.controller.js
@@ -1,9 +1,12 @@
-const { CartModel } = require('../models/cart')
-const { ProductModel } = require('../models/product')
+const { CartClient } = require('../models/cart-model')
+const { ProductClient } = require('../models/product-model')
+
+const cartClient = new CartClient()
+const productClient = new ProductClient()
 
 const createCart = async (req, res) => {
   try {
-    const cart = await CartModel.customCreation()
+    const cart = await cartClient.Cart.customCreation()
     return res.json(cart)
   } catch (error) {
     console.log(error.message)
@@ -15,7 +18,7 @@ const addProductToCart = async (req, res) => {
   try {
     const { id: cartId } = req.params
     const { productId } = req.body
-    const product = await ProductModel.findByStockID(productId)
+    const product = await productClient.Product.findByStockID(productId)
     if (!product) throw new Error('ERR_PRODUCT_NOT_FOUND')
 
     const cart = await CartModel.findById(cartId)
@@ -28,3 +31,4 @@ const addProductToCart = async (req, res) => {
 }
 
 module.exports = { createCart, addProductToCart }
+
