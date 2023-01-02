435ce94c69e2bbcda61bc17d06d3b8c966f2d5cf
Thotino GOBIN-GANSOU <gthomario@yahoo.com>
2023-01-02 17:31:29 +0100

    fix: a base class for the couchbase

--- db/client-base.js
+++ db/client-base.js
@@ -1,66 +1,47 @@
-const { connect, start, close } = require('ottoman')
+const { Ottoman } = require('ottoman')
 const EventEmitter = require('events')
 const { couchBaseUsername, couchbasePassword } = require('../config')
-
-// // Instantiate Ottoman
-// const { Ottoman, start } = require('ottoman')
-// const ottoman = new Ottoman({ collectionName: '_default' })
-// ottoman.connect({
-//   connectionString: 'couchbase://0.0.0.0:8091',
-//   bucketName: 'shop',
-//   username: couchBaseUsername,
-//   password: couchbasePassword
-// }).then(() => {
-//   start()
-// }).then(() => {
-//   console.log('Connected with the datastore')
-// }).catch((err) => {
-//   console.error(err)
-// })
 
 class CouchbaseClient extends EventEmitter {
   constructor(connectionOptions = {
     connectionString: 'couchbase://0.0.0.0:8091',
-    bucketName: 'sensefuel',
+    bucketName: 'shop',
     username: couchBaseUsername,
     password: couchbasePassword
   }) {
     super()
     this.options = connectionOptions
-    this.db = null
+    this.connection = null    
     // this.url = this.options.url
-    this.open = this.open.bind(this)
+    // this.open = this.open.bind(this)
     this.open()
   }
 
   async open() {
-    if (this.db) {
-      await this.close()
-      await this.open()
-    }
-    console.log({ ...this.options })
-    const connection = await connect({ ...this.options })
-    console.log({ ...connection })
-    // connection.on('error', (error) => { this.emit('error') })
-    this.db = connection
-    this.registerModels()
-    // connection.once('open', () => { this.emit('open') })
-    start()
-    this.emit('open')
+      // if (this.connection) {
+      //   await this.close()
+      //   await this.open()
+      // }
+      const ottoman = new Ottoman({ collectionName: '_default' })
+      this.connection = await ottoman.connect({ ...this.options })
+      this.connection.on('error', (error) => { this.emit('error') })
+      this.registerModels()
+      this.connection.start()
+      this.emit('open')  
   }
 
   async close() {
-    await close()
-    this.db.removeAllListeners()
-    this.db = null
+    await this.connection.close()
+    this.connection.removeAllListeners()
+    this.connection = null
     this.emit('close')
   }
   
   async reopen() {
-    if (this.db) {
-      await this.close()
-      await this.open()
-    }
+    // if (this.connection) {
+    //   await this.close()
+    //   await this.open()
+    // }
     return this.open();
   }
 
